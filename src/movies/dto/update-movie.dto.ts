2d161dabd55c737f78e9db870821a1ba00c3b370
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-26 20:54:57 +0900

    UpdateMovieDto 선언및 patch api 개선  - patch api URL 추가: /:id  - UpdateMovieDto 이용하여 기존 영화 데이터 변경  - install @nestjs/mapped-types 설치  - UpdatMovieDto 구현시 PartialType 함수를 상속을 이용하여 구현(모든 필드를 선택적으로 만들어줌)   - CreateDto => UpdateDto 쉽게 구현!!

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,21 +1,43 @@
-import { IsString, IsNumber, IsOptional } from 'class-validator';
+// import { IsString, IsNumber, IsOptional } from 'class-validator';
+//
+// export class UpdateMovieDto {
+//
+//   @IsString()
+//   title?: string;
+//
+//   @IsString()
+//   author?: string;
+//
+//   @IsNumber()
+//   year?: number;
+//
+//   @IsNumber()
+//   rating?: number;
+//
+//   @IsOptional()
+//   @IsString({ each: true })
+//   genre?: string[];
+//
+// }
 
-export class UpdateMovieDto {
 
-  @IsString()
-  title?: string;
+import { PartialType } from '@nestjs/mapped-types'
+import { CreateMovieDto } from './create-movie.dto';
 
-  @IsString()
-  author?: string;
 
-  @IsNumber()
-  year?: number;
+/*
+일반적으로 동일한 엔터티 유형에 대해 CreateDTO 및 UpdateDto 모두 빌드해야하는 실제 예제를 상상해 보겠습니다 .
+CreateDTO 에는 모든 필드가 필요할 수 있지만 UpdateDto는 모든 필드를 "선택 사항"으로 만들 수 있습니다 .
+말할 것도없이,이 두 유형은 엔티티 유형의 변형 일 수도 있습니다 (어느 정도까지).
+그것은 많은 중복 코드입니다!
+따라서 NestJS는 이제 유형 변환을 수행하는 여러 유틸리티 함수를 제공하여 이를 방지하고 삶을 조금 더 쉽게 만들 수 있습니다.
+사용 가능한 매핑 유형 :
+PartialType -입력 유형의 모든 속성이 "선택 사항"으로 설정된 유형 (클래스)을 반환합니다 (요구 사항 : 각 속성에 적어도 하나의 유효성 검사 데코레이터가 적용됨).
+PickType -입력 유형에서 속성 집합을 선택하여 새로운 유형 (클래스)을 생성합니다.
+OmitType -입력 유형에서 모든 속성을 선택한 다음 특정 키 세트를 제거하여 유형을 구성합니다.
+IntersectionType -두 가지 유형을 하나의 새로운 유형 (클래스)으로 결합
+이 기사 에서 더 많은 것을 읽으십시오 .
+ */
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
 
-  @IsNumber()
-  rating?: number;
 
-  @IsOptional()
-  @IsString({ each: true })
-  genre?: string[];
-
-}
