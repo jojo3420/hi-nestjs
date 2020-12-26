9b0e35d372ca58f6b1ff5f3113f110616a0e4636
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-26 21:23:18 +0900

    movies.module.ts 생성 및 구조 변화  - 무비 모듈 생성하여 컨트롤러, 서비스 setup  - app.module 에서 import 하여 movies.module 추가시킴

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/hi')
-  getSomething(): string {
-    return "hi Something 스페셜"
+  home(): string {
+    return this.appService.getHome()
   }
 }
