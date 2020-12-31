6dbbb14399cc35ce62d90188b88aee8d4a81689a
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-31 19:46:53 +0900

    fix  - CreateMoviesDto, UpdateMovieDto 생성자 제거하니 테스트코드 망가짐 => 수정완료

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -28,85 +28,84 @@ describe('MoviesService', () => {
 
   describe('Create', () => {
     it('Before Create', () => {
-      const allMovies: Movie[] = service.getAll()
-      expect(allMovies.length).toBe(2)
-    })
+      const allMovies: Movie[] = service.getAll();
+      expect(allMovies.length).toBe(2);
+    });
 
     it('Create Movie', () => {
-      const allMovies: Movie[] = service.getAll()
-      const newMovie = new CreateMovieDto('끝까지 간다', '김감독', 5, 2012, ['Action'])
-      const result = service.createMovie(newMovie)
+      const allMovies: Movie[] = service.getAll();
+      const newMovie: CreateMovieDto = { title: '끝까지 간다', author: '김감독', rating: 5, year: 2012, genre: ['Action'] };
+      const result = service.createMovie(newMovie);
       // @ts-ignore
       const { movie, created } = result;
-      expect(movie).toBeDefined()
-      expect(created).toBe(true)
-      expect(allMovies.length).toBe(3)
+      expect(movie).toBeDefined();
+      expect(created).toBe(true);
+      expect(allMovies.length).toBe(3);
     });
-  })
+  });
 
 
   describe('Read', () => {
     it('Read All', () => {
-      const movies: Movie[] = service.getAll()
-      expect(movies).toBeInstanceOf(Array)
-    })
+      const movies: Movie[] = service.getAll();
+      expect(movies).toBeInstanceOf(Array);
+    });
 
     it('Read Movie by ID', () => {
-      const movie: Movie = service.getOne(1)
-      expect(movie).toBeDefined()
-      expect(movie).toBeInstanceOf(Movie)
-      expect(movie.title).toEqual('해리포터1')
-    })
+      const movie: Movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie).toBeInstanceOf(Movie);
+      expect(movie.title).toEqual('해리포터1');
+    });
 
     it('throw 404 exception', () => {
       try {
-        service.getOne(999)
+        service.getOne(999);
       } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e).toBeInstanceOf(NotFoundException);
       }
-    })
-  })
+    });
+  });
 
   describe('Update', () => {
     it('Update Movie by ID', () => {
-      const movie: UpdateMovieDto = new UpdateMovieDto('Harry porter', '조앨_줌마', 1800);
-      service.modifyMovie(1, movie)
-      const target: Movie = service.getOne(1)
-      expect(target.title).toEqual(movie.title)
-      expect(target.author).toEqual(movie.author)
-      expect(target.year).toEqual(movie.year)
-    })
+      const movie: UpdateMovieDto = {title: 'Harry porter', author: '조앨_줌마', year: 1800 }
+      service.modifyMovie(1, movie);
+      const target: Movie = service.getOne(1);
+      expect(target.title).toEqual(movie.title);
+      expect(target.author).toEqual(movie.author);
+      expect(target.year).toEqual(movie.year);
+    });
 
     it('throw Notfound Exception', () => {
       try {
-        service.modifyMovie(9999, new UpdateMovieDto())
+        service.modifyMovie(9999, new UpdateMovieDto());
       } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e).toBeInstanceOf(NotFoundException);
       }
-    })
+    });
 
-  })
+  });
 
   describe('Delete', () => {
-    it ('Delete Movie By ID', () => {
-      const allMovies: Movie[] = service.getAll()
-      expect(allMovies.length).toBe(2)
-      const cnt: number = service.removeMovie(1)
-      expect(cnt).toBe(1)
-      expect(allMovies.length).toBe(1)
-    })
+    it('Delete Movie By ID', () => {
+      const allMovies: Movie[] = service.getAll();
+      expect(allMovies.length).toBe(2);
+      const cnt: number = service.removeMovie(1);
+      expect(cnt).toBe(1);
+      expect(allMovies.length).toBe(1);
+    });
 
-    it ('Throw Notfound Exception', () => {
+    it('Throw Notfound Exception', () => {
       try {
-        service.removeMovie(9999)
+        service.removeMovie(9999);
       } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e).toBeInstanceOf(NotFoundException);
       }
-    })
+    });
 
 
-  })
-
+  });
 
 
 });
